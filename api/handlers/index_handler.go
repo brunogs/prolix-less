250fa9dd76311c68ffb49051cd732ad2245223e9
Bruno Silva <bruno.silva@digibee.com>
2024-03-28 09:55:05 -0300

    add: htmx

--- api/handlers/index_handler.go
+++ api/handlers/index_handler.go
@@ -9,3 +9,8 @@ import (
 func (h GinHandler) Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
+
+func (h GinHandler) Video(c *gin.Context) {
+	//TODO replace video data
+	c.HTML(http.StatusOK, "video.html", gin.H{})
+}
--- api/handlers/setup_handler.go
+++ api/handlers/setup_handler.go
@@ -7,8 +7,11 @@ type GinHandler struct {
 }
 
 func (h GinHandler) SetupEndpoints(r *gin.Engine) {
+	// endpoints
 	r.GET("/", h.Index)
+	r.POST("/video", h.Video)
 
+	// static content
 	r.Static("/css", "web/static")
 	r.LoadHTMLGlob("web/templates/*")
 }
