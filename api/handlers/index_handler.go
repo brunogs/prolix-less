e8c25b33e00850d4ddc4d1ea450cb6ab65265db4
Bruno Silva <bruno.silva@digibee.com>
2024-03-28 23:26:42 -0300

    add: embed youtube video

--- api/handlers/index_handler.go
+++ api/handlers/index_handler.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +13,19 @@ func (h GinHandler) Index(c *gin.Context) {
 }
 
 func (h GinHandler) Video(c *gin.Context) {
-	//TODO replace video data
-	c.HTML(http.StatusOK, "video.html", gin.H{})
+	parameters := make(map[string]string)
+	if err := c.Bind(parameters); err != nil {
+		log.Println(err)
+		c.Status(http.StatusInternalServerError)
+	}
+	if videoUrl, ok := parameters["video-url"]; ok {
+		urlParsed, err := url.Parse(videoUrl)
+		if err != nil {
+			log.Println(err)
+			c.Status(http.StatusInternalServerError)
+		}
+		videoID := urlParsed.Query().Get("v")
+		parameters["videoID"] = videoID
+	}
+	c.HTML(http.StatusOK, "video.html", parameters)
 }
